b4ddfc3a1297bb3510ea2816edda241826ed0919
Daniel Fures Melo <daniel.melo@jointecnologia.com.br>
2022-08-28 23:01:29 -0300

    FRONT-D-003
    Reestruturação do front.

---
+++ web/js/login/index.js
@@ -0,0 +1,4 @@
+jQuery(function () {
+    // Implementar aqui js relacionado a lpagina login/index.html
+    // Aos poucos ir retirando do script.js
+});
---
+++ web/js/vagas/index.js
@@ -0,0 +1,4 @@
+jQuery(function () {
+    // Implementar aqui js relacionado a pagina vagas/index.html
+    // Aos poucos ir retirando do script.js
+});
