f6453d628a7b0aea782fef5ddb8aecb9ea9a6248
Daniel Fures Melo <daniel.melo@jointecnologia.com.br>
2022-10-04 21:41:18 -0300

    FRONTEND-D-005
    Integração com o backend. Tela de login

--- web/js/login/index.js
+++ web/js/login/index.js
@@ -1,4 +1,39 @@
 jQuery(function () {
-    // Implementar aqui js relacionado a lpagina login/index.html
-    // Aos poucos ir retirando do script.js
+    $("#login").click(function () {
+        login();
+    });
+    
 });
+
+
+function login() {
+    let matricula = document.getElementById("lemail").value;
+    let senha = document.getElementById("lpassword").value;
+
+    let mat = $("#lemail").val();
+    let pass = $("#lpassword").val();
+
+    console.log('Mat: '+matricula, 'Pass: '+senha, 'Mat2: '+mat, 'Pass2: '+pass);
+
+    alert('Entrou');
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/auth/login",
+        data: {
+            matricula: matricula,
+            senha: senha
+        },
+        success: function (data) {
+            console.log('Deu sucesso',data);
+            if (data.code == 0) {
+                window.location.href = "/index";
+            } else {
+                alert(data.msg);
+            }
+        },
+        error: function (data) {
+            console.log('Deu erro',data);
+            alert(data.msg);
+        }
+    });
+}
