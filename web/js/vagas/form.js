2b865762d7a1f295b432c234fd5623458edee618
Daniel Fures Melo <daniel.melo@jointecnologia.com.br>
2022-11-13 22:32:33 -0300

    FRONT-D-007
    Ajuste no dataTable
    Paginação tela de vagas
    Add plugin toastr
    Add plugin bootbox
    Implementa excluir
    Implementado editar.

--- web/js/vagas/form.js
+++ web/js/vagas/form.js
@@ -1,6 +1,6 @@
 jQuery(function () {
-    $(".areaAtuacao").chosen(); 
-    $('#modalCadastroVaga').modal({backdrop: 'static', keyboard: false});
+    $(".areaAtuacao").chosen();
+    $('#modalCadastroVaga').modal({ backdrop: 'static', keyboard: false });
 
     $("#btnCadastrarVaga").on('click', function () {
         cadastrarVaga();
@@ -12,8 +12,7 @@ jQuery(function () {
     });
 });
 
-function carregarTags() 
-{
+function carregarTags() {
     let token = localStorage.getItem('token');
 
     $.ajax({
@@ -26,19 +25,18 @@ function carregarTags()
             'Authorization': 'Bearer ' + token
         },
         success: function (data) {
-            $.map(data.content, function( tag ) {
+            $.map(data.content, function (tag) {
                 $('#area_atuacao').append(`<option value="${tag.id}">${tag.nome}</option>`);
             });
             $("#area_atuacao").trigger("chosen:updated");
         },
         error: function (data) {
-            alert('Erro: ' + data.responseJSON.erro);
+            toastr.error('Erro: ' + data.responseJSON.erro);
         }
     });
 }
 
-function cadastrarVaga() 
-{
+function cadastrarVaga() {
     let titulo = $("#titulo").val();
     let descricao = $("#descricao").val();
     let salario = $("#salario").val();
@@ -49,12 +47,14 @@ function cadastrarVaga()
     let tags = '';
     tags += "[";
 
-    for(var i = 0; i<areaAtuacao.length; i++) {
-        if(i+1==areaAtuacao.length) {
-            tags += JSON.stringify({id: areaAtuacao[i]});
+    //$(".spinner-wrapper").show();
+
+    for (let i = 0; i < areaAtuacao.length; i++) {
+        if (i + 1 == areaAtuacao.length) {
+            tags += JSON.stringify({ id: areaAtuacao[i] });
             tags += "]";
-        }else {
-            tags += JSON.stringify({id: areaAtuacao[i]}) + ", ";
+        } else {
+            tags += JSON.stringify({ id: areaAtuacao[i] }) + ", ";
         }
     }
 
@@ -79,17 +79,113 @@ function cadastrarVaga()
             'Authorization': 'Bearer ' + token
         },
         success: function (data) {
-            window.location.href = "../vagas/index.html";
+            $('#modalCadastroVaga').modal('hide');
+            $('#data_table_vagas').DataTable();
+            listar();
         },
         error: function (data) {
-            alert('Erro: ' + data.responseJSON.erro);
+            alert('Error: ' + data.responseJSON.erro);
+        },
+        always: function () {
+            // alert('Fucnioou');
+            // $(".spinner-wrapper").hide();
         }
     });
 }
 
-function limparCampos() 
-{
-    $('#formCadastrarVaga').each (function(){
+function excluir(id) {
+    let token = localStorage.getItem('token');
+
+    // Falta verificar o motivo de n poder excluir
+    // tb o X do modal confirm estar alinhado a esquerda
+    appUtil.confirmBox('Deseja realmente excluir essa vaga???', function (retorno) {
+        if (retorno) {
+            appUtil.showLoader();
+            $.ajax({
+                type: "DELETE",
+                url: `${url}/vaga/deletar/${id}`,
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+                success: function (data) {
+                    console.log(data);
+                    toastr.success("Registro excluído com sucesso!");
+                    listar();
+                },
+                error: function (data) {
+                    // appUtil.createFlashMesseger('Deu ruim', 401, '#flashMensager');
+                    console.log(data);
+                    toastr.options.closeButton = true;
+                    toastr.options.closeMethod = 'fadeOut';
+                    toastr.options.preventDuplicates = true;
+                    toastr.options.progressBar = true;
+                    toastr.error("Erro ao tentar excluir o registro.", 'Error', { timeOut: 5000 });
+                    //alert('Error: ' + data.responseJSON.erro);
+                },
+                always: function () {
+                    appUtil.hideLoader();
+                }
+            });
+
+        }
+    });
+}
+
+function editar(id, titulo, descricao, institucional, dataCriacao, salario, link, tags) {
+    console.log('id ' + id, 'titulo ' + titulo, 'descricao ' + descricao, 'institucional ' + institucional, 'dataCriacao ' + dataCriacao, 'salario ' + salario, 'link ' + link, 'tags ' + tags);
+
+    return false;
+
+    limparCampos();
+    $("#titulo").val(titulo);
+    $("#descricao").val(descricao);
+    $("#salario").val(salario);
+    $("#link").val(link);
+    $("#id").val(id);
+    $("#institucional").val(institucional);
+    $("#dataCriacao").val(dataCriacao);
+
+    carregarTags();
+
+    for (let i = 0; i < tags.length; i++) {
+        $("#area_atuacao").val(tags[i].id);
+        $("#area_atuacao").trigger("chosen:updated");
+    }
+
+    $('#modalCadastroVaga').modal('show');
+
+    $("#btnCadastrarVaga").on('click', function () {
+        atualizarVaga();
+    });
+
+
+
+}
+
+function atualizarVaga(data) {
+    let token = localStorage.getItem('token');
+
+    $.ajax({
+        type: "GET",
+        url: `${url}/vaga/editar/${id}`,
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        },
+        success: function (data) {
+            console.log(data);
+            toastr.success("Registro atualizado com sucesso!");
+            listar();
+        },
+        error: function (data) {
+            toastr.error('Erro: ' + data.responseJSON.erro);
+        }
+    });
+}
+
+function limparCampos() {
+    $('#formCadastrarVaga').each(function () {
         this.reset();
     });
 }
--- web/js/vagas/index.js
+++ web/js/vagas/index.js
@@ -1,10 +1,9 @@
-
 jQuery(function () {
+    $('#data_table_vagas').DataTable();
     listar();
 });
 
-function listar() 
-{
+function listar() {
     let token = localStorage.getItem('token');
 
     $.ajax({
@@ -17,7 +16,6 @@ function listar()
             'Authorization': 'Bearer ' + token
         },
         success: function (data) {
-            console.log('aqui', data.content.length, data);
             let html = '';
             let botoes = '';
             let perfil = localStorage.getItem('perfil') ?? 'admin';
@@ -25,54 +23,72 @@ function listar()
             if (data.content.length > 0) {
                 data.content.forEach(function (item) {
 
-                if (perfil == 'admin' || perfil == 'professor') {
-                    botoes = `
-                        <a title="Editar">
-                            <button class="btn btn-info" onclick="editar(${item.id})">
-                                <em class="fa fa-edit"></em> 
-                            </button>
-                        </a>
-                    `;
+                    if (perfil == 'admin' || perfil == 'professor') {
 
-                    botoes += `
-                        <a title="Suspender">
-                            <button class="btn btn-warning" onclick="suspender(${item.id})">
-                                <em class="fa fa-power-off"></em> 
-                            </button>
-                        </a>
-                    `;
+                        botoes = `
+                            <a title="Editar">
+                                <button class="btn btn-info" onclick="editar(${item.id, item.titulo, item.descricao, item.institucional, item.dataCriacao, item.salario})">
+                                    <em class="fa fa-edit"></em> 
+                                </button>
+                            </a>
+                        `;
 
-                    botoes += `
-                        <a title="Excluir">
-                            <button class="btn btn-danger" onclick="excluir(${item.id})">
-                                <em class="fa fa-trash"></em> 
-                            </button>
-                        </a>
-                    `;
-                } else if(perfil == 'aluno') {
-                    botoes = `
-                        <a title="Candidatar"> 
-                            <button class="btn btn-success" onclick="excluir(${item.id})">
-                                Candidatar-se
-                            </button>
-                        </a>
-                    `;
-                } else {
-                    botoes += '<a class="btn btn-danger">Deu ruim</a>';
-                }
+                        botoes += `
+                            <a title="Suspender">
+                                <button class="btn btn-warning" onclick="suspender(${item.id})">
+                                    <em class="fa fa-power-off"></em> 
+                                </button>
+                            </a>
+                        `;
 
-                    html += `<tr class="trCss">
+                        botoes += `
+                            <a title="Excluir">
+                                <button class="btn btn-danger" onclick="excluir(${item.id})">
+                                    <em class="fa fa-trash"></em> 
+                                </button>
+                            </a>
+                        `;
+                    } else if (perfil == 'aluno') {
+                        botoes = `
+                            <a title="Candidatar"> 
+                                <button class="btn btn-success" onclick="excluir(${item.id})">
+                                    Candidatar-se
+                                </button>
+                            </a>
+                        `;
+                    } else {
+                        botoes += '<a class="btn btn-danger">Você não possui permissão</a>';
+                    }
+
+                    html += ` <tr class="trCss">`;
+                    html += `     <td>${item.titulo}</td>`;
+                    html += `     <td>${item.descricao}</td>`;
+                    html += `     <td>${item.institucional}</td>`;
+                    html += `     <td>${item.dataCriacao}</td>`;
+                    html += `     <td>${item.salario ? 'R$' + item.salario : 'A combinar'}</td>`;
+                    html += `     <td class="acoes"> ${botoes} </td>`;
+                    html += ` </tr>`;
+
+                    /* html += `<tr class="trCss">
                                 <td>${item.titulo}</td>
                                 <td>${item.descricao}</td>
                                 <td>${item.institucional}</td>
                                 <td>${item.dataCriacao}</td>
-                                <td>${item.salario}</td>
+                                <td>${item.salario ? 'R$' + item.salario : 'A combinar'}</td>
                                 <td>${botoes}</td>
-                            </tr>`;
+                            </tr>`; */
                 });
-                $('#tBodyIdVagas').html(html);
+
+                $('#data_table_vagas').DataTable().destroy();
+                $('#data_table_vagas tbody').html(html);
+                $('#data_table_vagas').DataTable();
+
+
+                //$('#tBodyIdVagas').html(html);
+                //
+                //$('#data_table_vagas').DataTable();
             }
-            
+
         },
         error: function (data) {
             alert('Erro: ' + data.responseJSON.erro);
